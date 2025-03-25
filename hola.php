4ec461ac9a2155b045c445a73d033311048cfb7a
Ruben Fernando <ruferesfer@gmail.com>
2025-03-24 22:26:16 -0600

    Código básico php

---
+++ hola.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "Hola mundo, soy Rubén Fernando Espinoza";
+
+?>
+
+<img src="/Actividad3/proyecto_php/Programmer penguin.jpg" 
+style="width: 150px; height: auto; display: block; margin-top: 10px;">
